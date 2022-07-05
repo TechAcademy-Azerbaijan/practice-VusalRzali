f65e8949ed9eb3fede8e3c10a5f04c2cf7b60d5c
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-05 11:50:50 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,11 @@
 const prompt = require('prompt');
 prompt.start();
 
-
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+    let arr = result.input.split(',');
+    let arr2= arr.reverse()
+    for(i = 0; i<arr.length; i++){
+        console.log(arr2[i]);
+    }
+
+})
