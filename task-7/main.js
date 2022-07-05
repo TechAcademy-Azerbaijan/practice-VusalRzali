2e552d30a5fd3a7587206e93360bb4401928765d
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-05 13:02:59 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -1,9 +1,11 @@
 const prompt = require('prompt');
 prompt.start();
 
-
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+    let n =parseInt(result.input)
+    let ch = "";
+for (let i = 1; i * i < n; i++) {
+  ch = ch + `${i * i}` + " ";
+}
+console.log(ch);
+})
