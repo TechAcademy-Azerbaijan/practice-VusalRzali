b33fd9ad82bafdfdef6cfeaa57d329415d5d9257
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-05 12:41:11 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,19 @@
 const prompt = require('prompt');
 prompt.start();
 
-
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+    let Fibonacci6 = parseInt(result.input)
+        let sayi1 = 0;
+        let sayi2 = 1;
+        let toplam;
+        let fibonacciSayilari = [];
+ 
+        for(let i = 0; i < Fibonacci6; i++){
+            toplam = sayi1 + sayi2;
+            sayi1 = sayi2;
+            sayi2 = toplam;
+            fibonacciSayilari.push(toplam);
+        }        
+ 
+        console.log("Fibonacci (" + Fibonacci6 + ") : " + toplam);
+})
