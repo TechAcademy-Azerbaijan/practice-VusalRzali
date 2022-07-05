65bf676cf4737bba6658b817ea259f291322ee83
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-05 12:58:48 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,16 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let a = 1,
+b = 2,
+c = 3,
+d = 4;
+if (a >= b && a >= c && a >= d) {
+console.log(a);
+}
+if (b >= a && b >= c && b >= d) {
+console.log(b);
+}
+if (c >= b && c >= a && c >= d) {
+console.log(c);
+}
+if (d >= b && d >= c && d >= a) {
+console.log(d);
+}
